257a776053cd37d80844cd7983826f201c20b684
Zerkius <alexy.tiv@gmail.com>
2020-11-23 11:23:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,13 +4,13 @@ self.addEventListener('install', (e) => {
 
 var cacheName = 'GinkoBus-v1';
 var contentToCache = [];
-var appShellFile=[];
 var iconsSize=["32","64","96","128","168","180","192","256","512"];
 
 for(var i=0; i<iconsSize.length; i++) {
     contentToCache.push('icons/icon-'+iconsSize[i]+'.png');
 }
 
+contentToCache.push("index.html");
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
@@ -22,6 +22,17 @@ self.addEventListener('install', (e) => {
     );
   });
 
-self.addEventListener('fetch', (e) => {
-    console.log('[Service Worker] Fetched resource '+e.request.url);
-});
+  self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: '+e.request.url);
+        return r || fetch(e.request).then((response) => {
+                  return caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching new resource: '+e.request.url);
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  });
