d755d9b383b164af62f64bf15c51d32d9dfc7290
Zerkius <alexy.tiv@gmail.com>
2020-11-23 11:44:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,13 +13,19 @@ for(var i=0; i<iconsSize.length; i++) {
 contentToCache.push("index.html");
 contentToCache.push("app.js");
 
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil(
-      caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching all: app shell and content');
-        return cache.addAll(contentToCache);
-      })
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.open('mysite-dynamic').then(function (cache) {
+        return cache.match(event.request).then(function (response) {
+          return (
+            response ||
+            fetch(event.request).then(function (response) {
+              cache.put(event.request, response.clone());
+              return response;
+            })
+          );
+        });
+      }),
     );
   });
 
