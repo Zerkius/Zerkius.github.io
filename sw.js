a39190c86c7bbd246b82a6293469d847923d378e
Zerkius <alexy.tiv@gmail.com>
2020-11-23 16:42:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,4 @@
 
-var cacheName = 'GinkoBus-v1';
-var contentToCache = [];
-var iconsSize=["32","64","96","128","168","180","192","256","512"];
-
-for(var i=0; i<iconsSize.length; i++) {
-    contentToCache.push('icons/icon-'+iconsSize[i]+'.png');
-}
-
-contentToCache.push("index.html");
-contentToCache.push("app.js");
-
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil(
-      caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching all: app shell and content');
-        return cache.addAll(contentToCache);
-      })
-    );
-  });
 
 self.addEventListener('fetch', function (event) {
   event.respondWith(
