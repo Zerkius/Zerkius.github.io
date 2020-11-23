35248f137495021786cf97ce7673621387dc04ed
Zerkius <alexy.tiv@gmail.com>
2020-11-23 11:46:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,13 +17,11 @@ self.addEventListener('fetch', function (event) {
     event.respondWith(
       caches.open('mysite-dynamic').then(function (cache) {
         return cache.match(event.request).then(function (response) {
-          return (
-            response ||
-            fetch(event.request).then(function (response) {
-              cache.put(event.request, response.clone());
-              return response;
-            })
-          );
+          var fetchPromise = fetch(event.request).then(function (networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+          return response || fetchPromise;
         });
       }),
     );
