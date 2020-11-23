b1bc657f58e9efe014c333db044314afdad0ebd0
Zerkius <alexy.tiv@gmail.com>
2020-11-23 11:53:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,5 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-});
 
-self.addEventListener('fetch', function (event) {
+  self.addEventListener('fetch', function (event) {
     event.respondWith(
       caches.open('mysite-dynamic').then(function (cache) {
         return cache.match(event.request).then(function (response) {
@@ -15,20 +12,5 @@ self.addEventListener('fetch', function (event) {
           );
         });
       }),
-    );
-  });
-
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((r) => {
-            console.log('[Service Worker] Fetching resource: '+e.request.url);
-        return r || fetch(e.request).then((response) => {
-                  return caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching new resource: '+e.request.url);
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        });
-      })
     );
   });
