2d4449ba713f2e2320e976f9d85bb4654a9dee0c
Zerkius <alexy.tiv@gmail.com>
2020-11-23 11:58:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,37 @@
 
-  self.addEventListener('fetch', function (event) {
-    event.respondWith(
-      caches.open('mysite-dynamic').then(function (cache) {
-        return cache.match(event.request).then(function (response) {
-          return (
-            response ||
-            fetch(event.request).then(function (response) {
-              cache.put(event.request, response.clone());
-              return response;
-            })
-          );
-        });
-      }),
+var cacheName = 'GinkoBus-v1';
+var contentToCache = [];
+var iconsSize=["32","64","96","128","168","180","192","256","512"];
+
+for(var i=0; i<iconsSize.length; i++) {
+    contentToCache.push('icons/icon-'+iconsSize[i]+'.png');
+}
+
+contentToCache.push("index.html");
+contentToCache.push("app.js");
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+      caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all: app shell and content');
+        return cache.addAll(contentToCache);
+      })
     );
   });
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.open('mysite-dynamic').then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return (
+          response ||
+          fetch(event.request).then(function (response) {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+        );
+      });
+    }),
+  );
+});
